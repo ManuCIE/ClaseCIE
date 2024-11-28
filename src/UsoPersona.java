5e6c7c328a62e343d2da04530bb486ac73f31711
ManuCIE <ciervo.arco.iris@gmail.com>
2024-11-28 13:15:59 +0100

    Primera Subida VS

---
+++ src/UsoPersona.java
@@ -0,0 +1,18 @@
+public class UsoPersona {
+    public static void main(String[] args) {
+
+        Persona paula = new Persona();
+        paula.edad = 20;
+        paula.nombre = "Paula";
+        paula.altura = 1.57;
+        System.out.println(paula.nombre + ", " + paula.edad + ". Altura: " + paula.altura + "cm");
+
+        Persona manu = new Persona();
+        manu.edad = 28;
+        manu.nombre = "Manu";
+        manu.altura = 1.73;
+        System.out.println(manu.nombre + ", " + manu.edad + ". Altura: " + manu.altura + "cm");
+
+    }
+
+}
